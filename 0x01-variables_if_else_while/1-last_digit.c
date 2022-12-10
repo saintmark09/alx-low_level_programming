ba4061f7b7f3dc65507e52fbbc45e971ecc5bbe6
saintmark09 <markoriabure@gmail.com>
2022-12-09 22:27:54 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,9 +6,6 @@
  * main - the string last digit of, followed by
  * n, followed by
  * the string is, followed by
- * if the last digit of n is greater than 5: the string and is greater than 5
- * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
  *
  * Return: Always 0 (Success)
  */
