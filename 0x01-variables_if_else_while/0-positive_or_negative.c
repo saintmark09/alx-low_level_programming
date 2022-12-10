034813382db64cd5751f7856b13dc1da969cdbd8
saintmark09 <markoriabure@gmail.com>
2022-12-09 20:50:56 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,9 @@
  * if the number is greater than 0: is positive
  * if the number is 0: is zero
  * if the number is less than 0: is negative
- * return: 0
+ *
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
