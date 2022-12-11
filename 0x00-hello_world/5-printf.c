d66cb4d7a3d12dd2c9e528ff1878ff5e35d1a6c8
saintmark09 <markoriabure@gmail.com>
2022-12-10 21:48:57 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Write a C program that prints exactly
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
