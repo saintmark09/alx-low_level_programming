cc2ab557e831ace06be666a2241ea85cee710e6b
saintmark09 <markoriabure@gmail.com>
2022-12-19 15:26:57 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int as parameter
+ * @n: Entry point
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My C-pointers, arrats and strings
